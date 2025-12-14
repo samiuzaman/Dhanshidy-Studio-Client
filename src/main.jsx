04ad3483d6be9d81718ab05d6e6089eba3c10518
samiuzaman <chowdhurysamiuzzaman@gmail.com>
2025-12-14 16:45:56 +0600

    Solve Routing Problem

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import Router from "./Routes/router.jsx";
+import Router from "./Routes/Router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
